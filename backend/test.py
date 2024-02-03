1436d8b1536c57ba914b70f7bc99f7985f549816
yifanhao <yifanhao@students.cs.ubc.ca>
2024-02-03 10:17:22 -0800

    test

---
+++ backend/test.py
@@ -0,0 +1,8 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print('hello world')
