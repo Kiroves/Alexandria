fec8b7116822c578af222978f00a56500b0686ec
yifanhao <yifanhao@students.cs.ubc.ca>
2024-02-03 16:22:55 -0800

    ai sucks

---
+++ backend/main.py
@@ -0,0 +1,125 @@
+import time
+from pinecone import Pinecone, ServerlessSpec
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Pinecone as PC
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API')
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = "alexandria-test-2"
+
+def create_index(pc, index_name):
+    spec = ServerlessSpec(
+        cloud="aws",
+        region="us-west-2"
+    )
+
+    if index_name in pc.list_indexes().names():
+        pc.delete_index(index_name)
+
+    pc.create_index(
+        index_name,
+        dimension=1536,  # dimensionality of text-embedding-ada-002
+        metric='cosine',
+        spec=spec
+    )
+
+    while not pc.describe_index(index_name).status['ready']:
+        print("waiting for index to be ready")
+        time.sleep(1)
+
+# def create_embeddings(vectorstore, pdf_path):
+
+
+
+
+# create_index(pc, index_name)
+
+
+index = pc.Index(index_name)
+
+# print(index)
+
+# create_embeddings(vectorstore, "./data/companion.pdf")
+
+loader = PyPDFLoader("./data/companion.pdf")
+
+pages = loader.load_and_split()
+
+page_contents = [page.page_content for page in pages]
+
+# vectorstore.add_texts([page.page_content for page in pages])
+
+docs = []
+
+pages = pages[3:10]
+
+# combine all pages into one list  of strings
+
+# page_content = " ".join([page.page_content for page in pages])
+
+# print(page_content)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=80)
+texts = text_splitter.split_documents(pages)
+
+
+print(texts)
+
+
+
+
+# print(page00)
+
+embed = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+vectorstore = PC(
+    index, embed, "text"
+)
+
+for i, text in enumerate(texts):
+    embedding = embed.embed_query(text.page_content)
+    metadata = {
+        "text": text.page_content,
+    }
+    docs.append((str(i), embedding, metadata))
+
+# docs.append(("0", embed.embed_query(page_content), {"text": page_content}))
+
+
+index.upsert(docs, batch_size=32)
+
+
+
+
+
+# index.upsert(page_contents)
+
+# for batch in page_contents.iter_documents(batch_size=100):
+
+
+query = "what is a combination circuit?"
+
+llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+)
+
+print(qa.invoke(query))
--- backend/test.py
+++ backend/test.py
@@ -3,6 +3,58 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
 
+from langchain_community.vectorstores import Pinecone as PC
+from langchain.embeddings.openai import OpenAIEmbeddings
+from pinecone import Pinecone
+
+from langchain_openai import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+
 load_dotenv()
 
-print('hello world')
+OPENAI_API_KEY = os.getenv('OPENAI_API')
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_API_ENV = os.getenv('PINECONE_API_ENV') 
+
+loader = PyPDFLoader("./data/companion.pdf")
+
+
+pages = loader.load_and_split()
+
+print(pages[6].page_content)
+
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=80)
+# texts = text_splitter.split_documents(pages)
+
+# print(texts[6])
+
+# pc = Pinecone(
+#     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+# )
+# index_name = "alexandria"
+
+# index = pc.Index(index_name)
+
+
+# embed = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+# text_field = "text"
+
+# # switch back to normal index for langchain
+# vectorstore = PC(
+#     index, embed.embed_query, text_field
+# )
+
+# query = "This course is divided into how many sections?"
+
+# llm = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+
+
+# qa = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     chain_type="stuff",
+#     retriever=vectorstore.as_retriever()
+# )
+
+# print(qa.invoke(query))
